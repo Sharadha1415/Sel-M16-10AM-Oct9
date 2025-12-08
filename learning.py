d771b985b3d64e620385a8a877eafc1a551a6072
Sharada1415 <ramyar.alpha@gmail.com>
2025-12-08 11:53:58 +0530

    defined even_odd() func

--- learning.py
+++ learning.py
@@ -1,6 +1,9 @@
-for num in range(10, 20):
-    if num%2==0:
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd")
+def even_odd(start, end):
+    for num in range(start, end):
+        if num%2==0:
+            print(f"{num} is even")
+        else:
+            print(f"{num} is odd")
+
+even_odd(100, 200)
 
